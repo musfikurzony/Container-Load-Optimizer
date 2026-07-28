38aa8dfb8bb6b5c252be2b6e7e5eebb2dd8c2f95
musfikurzony <musfikurzony@gmail.com>
2026-07-28 17:44:17 +0600

    Add files via upload

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Container-Load-Optimizer/sw.js', { scope: '/Container-Load-Optimizer/' })})}
